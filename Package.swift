478c15845aa612686dce3c9a3c34103b0fee6568
AbdelAli <hello@abdelali.me>
2020-07-14 12:14:47 +0100

    Clean package file

--- Package.swift
+++ Package.swift
@@ -5,27 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Unboxing",
-    platforms: [
-      .iOS(.v13)
-    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Unboxing",
-            targets: ["Unboxing"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "Unboxing", targets: ["Unboxing"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Unboxing",
-            dependencies: []),
-        .testTarget(
-            name: "UnboxingTests",
-            dependencies: ["Unboxing"]),
-    ]
+        .target(name: "Unboxing", dependencies: []),
+        .testTarget(name: "UnboxingTests", dependencies: ["Unboxing"]),
+    ],
+    swiftLanguageVersions: [.v5]
 )
